857f8624ee49158f21d6d56f4af104042584403e
Aleksands <aleksandrs.vaguscenko@gmail.com>
2020-11-21 16:10:44 +0200

    Add employee and person class

---
+++ OrderManagerClassLibrary/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagerClassLibrary
+{
+    [Serializable]
+    public class Customer : Person
+    {
+        public Customer()
+        {
+
+        }
+
+        public Customer(string name, string surname, string personalCode, string email)
+        {
+            Name = name;
+            Surname = surname;
+            PersonalCode = personalCode;
+            Email = email;
+        }
+
+    }
+}
---
+++ OrderManagerClassLibrary/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagerClassLibrary
+{
+    [Serializable]
+    public class Employee : Person
+    {
+        public Employee()
+        {
+
+        }
+        public Employee(string name, string surname, string personalCode, string email)
+        {
+
+            Name = name;
+            Surname = surname;
+            PersonalCode = personalCode;
+            Email = email;
+            AgreementDate = DateTime.Now;
+            AgreementNr = AgreementNumberGenerator();
+
+        }
+
+
+        public DateTime AgreementDate { get; }
+        public string AgreementNr { get; }
+
+        public override string ToString()
+        {
+            return base.ToString() + "\n\tAgreement Date: " + AgreementDate + "\n\tAgreement NR: " + AgreementNr;
+        }
+
+        private static string AgreementNumberGenerator()
+        {
+            Random random = new Random();
+            return random.Next(1000, 100000).ToString();
+        }
+
+    }
+}
